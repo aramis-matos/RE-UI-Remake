fba509f564bf8b1b006377ffe27a6a42f17b735a
zwesten <zwesten@clemson.edu>
2024-06-21 14:09:53 -0400

    changing to image

--- src/popups/HelpModalOnPage.jsx
+++ src/popups/HelpModalOnPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import img1 from "./contactUsPage.jpg"; // Import the image file
+import ContactModalOnPage from "./ContactModalOnPage"; // Assuming you have a ContactModalOnPage component
 
 const HelpModalOnPage = ({ isOpen, onClose, children }) => {
   const [showImage, setShowImage] = useState(false);
@@ -13,25 +14,30 @@ const HelpModalOnPage = ({ isOpen, onClose, children }) => {
   return (
     <div className="help-page-modal-overlay">
       <div className="help-page-modal-content">
-        <div className="contact-modal">
-          <button
-            className="contact-page"
-            id="contactButton"
-            data-testid="openContact"
-            onClick={toggleImage}
-          >
-            Contact Us
-          </button>
-          {showImage && (
-            <div className="image-container">
-              <img src={img1} alt="Contact Us" />
-            </div>
-          )}
-        </div>
-        <button className="help-page-close-button" onClick={onClose}>
-          &times;
-        </button>
-        {children}
+        {showImage ? (
+          <div className="image-container">
+            <img src={img1} alt="Contact Us" />
+            <button className="help-page-close-button" onClick={toggleImage}>
+              Back to Help
+            </button>
+          </div>
+        ) : (
+          <div className="contact-modal">
+            <button
+              className="contact-page"
+              id="contactButton"
+              data-testid="openContact"
+              onClick={toggleImage}
+            >
+              Contact Us
+            </button>
+            <button className="help-page-close-button" onClick={onClose}>
+              back
+            </button>
+            {children}
+          </div>
+        )}
+        <ContactModalOnPage isOpen={showImage} onClose={toggleImage} />
       </div>
     </div>
   );
