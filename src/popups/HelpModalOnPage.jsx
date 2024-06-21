258f54a00082bcb3ce20fd0d5c94ee5bccd73c81
zwesten <zwesten@clemson.edu>
2024-06-21 11:43:34 -0400

    contact initial commit

--- src/popups/HelpModalOnPage.jsx
+++ src/popups/HelpModalOnPage.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import img1 from "./contactUsPage.jpg"; // Import the image file
 
+const HelpModalOnPage = ({ isOpen, onClose, children }) => {
+  const [showImage, setShowImage] = useState(false);
 
-const HelpModalOnPage = ({isOpen, onClose, children}) => {
-    if (!isOpen) return null;
+  const toggleImage = () => {
+    setShowImage(!showImage);
+  };
 
+  if (!isOpen) return null;
 
   return (
     <div className="help-page-modal-overlay">
-    <div className="help-page-modal-content">
-      <button className="help-page-close-button" onClick={onClose}>
-        &times;
-      </button>
-      {children}
+      <div className="help-page-modal-content">
+        <div className="contact-modal">
+          <button
+            className="contact-page"
+            id="contactButton"
+            data-testid="openContact"
+            onClick={toggleImage}
+          >
+            Contact Us
+          </button>
+          {showImage && (
+            <div className="image-container">
+              <img src={img1} alt="Contact Us" />
+            </div>
+          )}
+        </div>
+        <button className="help-page-close-button" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default HelpModalOnPage
+export default HelpModalOnPage;
