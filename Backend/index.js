974ef01701f156de8de91626943a92ed1378e764
johnahunter1 <141785895+johnahunter1@users.noreply.github.com>
2024-07-15 14:29:38 -0400

    Established Frontend to Backend connection, Backend to Database connection, and created function to insert TS objects into MySQL Database.

---
+++ Backend/index.js
@@ -0,0 +1,86 @@
+// "index.js"
+// Created by Jack Hunter
+// 07-10-2024
+
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+import helmet from "helmet";
+import {
+  fileheader,
+  imagesubheader,
+  graphicsubheader,
+  textsubheader,
+  dessubheader
+} from "./TableResources.js";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.all("/*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+// route for "get" requests to "/" (root/homepage)
+app.get("/", (req, res, next) => {
+  res.json({ msg: "Message from the backend!!" });
+  // backend response
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080!");
+  // backend server listening for requests on port 8080
+});
+
+// INSERT TS OBJECTS INTO DB
+
+let headers = [
+  fileheader,
+  imagesubheader,
+  graphicsubheader,
+  textsubheader,
+  dessubheader
+];
+let headerNames = [
+  "fileheader",
+  "imagesubheader",
+  "graphicsubheader",
+  "textsubheader",
+  "dessubheader"
+];
+async function insertData() {
+  try {
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Password!",
+      database: "db",
+    });
+    let i = 0;
+    for (const element of headers) {
+      for (const obj of element) {
+        // data insertion loop
+        const sql = `INSERT INTO ${headerNames[i]} VALUES ('${obj.fieldName}','${obj.longName}',${obj.editable})`;
+        // Execute INSERT statement
+        connection.query(sql);
+        console.log(sql);
+      }
+      i++;
+    };
+    console.log('Data Inserted Successfully!');
+    connection.end();
+  }
+  catch (error) {
+    console.error('Error inserting data:', error);
+  }
+}
+// insertData();
+
+// INSERT TS OBJECTS INTO DB
