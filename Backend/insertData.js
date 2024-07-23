929e9fff8965dc2feeece5459eb92b0d248e801a
johnahunter1 <141785895+johnahunter1@users.noreply.github.com>
2024-07-23 08:28:34 -0400

    more gitignore + moved insertData() to different file

---
+++ Backend/insertData.js
@@ -0,0 +1,37 @@
+export async function insertData() {
+  let headers = [
+  fileheader,
+  imagesubheader,
+  graphicsubheader,
+  textsubheader,
+  dessubheader,
+];
+let headerNames = [
+  "fileheader",
+  "imagesubheader",
+  "graphicsubheader",
+  "textsubheader",
+  "dessubheader",
+];
+const treName = Object.keys(tres);
+  try {
+    let i = 0;
+    for (const element of headers) {
+      for (const obj of element) {
+        const sql = `INSERT INTO ${headerNames[i]} VALUES ('${obj.fieldName}','${obj.longName}',${obj.editable})`;
+        connection.query(sql);
+      }
+      i++;
+    }
+    for (i = 0; i < treName.length; i++) {
+      for (const element of tres[treName[i]]) {
+        const sql = `INSERT INTO tre VALUES ('${treName[i]}','${element.fieldname}',"${element.longname}",${element.editable})`;
+        connection.query(sql);
+      }
+    }
+    console.log("Data Inserted Successfully!");
+    connection.end();
+  } catch (error) {
+    console.error("Error inserting data:", error);
+  }
+}
